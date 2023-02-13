ac6cc554ead4cf84e3537ec0bd7473b0ecd54520
marcinhu <marcio_gomes11@hotmail.com>
2023-02-13 10:44:03 +0000

    fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Jeremy#0004 <https://github.com/ansljeremy>'
+
 description 'm-Forgery - Credits to original: Jeremy#0004 <https://github.com/ansljeremy>'
 version '1.0.0'
 
-client_script 'client/**.lua'
+client_script {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    "client/**.lua",
+}
+
 server_script 'server/**.lua'
 shared_script 'configs/**.lua'
